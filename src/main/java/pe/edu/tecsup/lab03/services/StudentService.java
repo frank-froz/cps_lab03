0df4aa14855628360b3ee802cc64fc0ffbf21cf0
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-04-03 17:45:35 -0500

    modificando clase

--- src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
+++ src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
@@ -1,4 +1,13 @@
 package pe.edu.tecsup.lab03.services;
 
 public class StudentService {
+    public function map()
+    {
+        Route::group([
+            'namespace' => $this->namespace,
+            'prefix' => 'students',
+        ], function ($router) {
+        require __DIR__ . '/routes.php';
+    });
+    }
 }
